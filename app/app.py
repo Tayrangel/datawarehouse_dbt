c78cdfb429e941a145ae95f043078d0b6d768305
Tayhana Rangel <tayhanarangel@gmail.com>
2025-01-06 18:54:35 -0300

    UPDATE: dashboard with more charts

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 #import
 import pandas as pd
+import numpy as np
 import os
 import streamlit as st
 from dotenv import load_dotenv
@@ -39,4 +40,23 @@ st.write('Here you will find information about commodities')
 
 df = get_data()
 
-st.dataframe(df)
+#Line chart
+df['extract_dt'] = pd.to_datetime(df['extract_dt']).dt.strftime('%Y-%m-%d') #'extract_dt' to date
+df_pivot = df.pivot_table(index='extract_dt', columns='symbol', values='profit_vl', aggfunc='sum') #Grouping the data to have column for 'symbol' and 'profit_vl'
+
+st.subheader("Commodities by profit")
+st.line_chart(df_pivot)
+
+#Bar chart and table
+df_grouped = df.groupby(['extract_dt', 'action']).agg({'total_vl': 'sum'}).reset_index() #Grouping the data by 'extract_dt' and 'action'
+df_pivot_action = df_grouped.pivot_table(index='extract_dt', columns='action', values='total_vl', aggfunc='sum', fill_value=0) #Pivot table to have columns for 'action'
+
+col1, col2 = st.columns(2) #Layout to show the bar chart and the table
+
+with col1:
+    st.subheader("Buying and selling commodity stocks")
+    st.bar_chart(df_pivot_action)
+
+with col2:
+    st.subheader("Commodities table")
+    st.dataframe(df, use_container_width=True) #Table with all width of the container
